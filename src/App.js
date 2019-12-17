0f9c78479a23a3c1d7b0d2e40a81d4f39dd26884
VinayModepalli <vinaykumarmodepalli@gmail.com>
2019-12-18 00:23:43 +0530

    Import one Component into Another Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Avengers from './Avengers';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My First App</h1>
-      <p>Happy Coding..!</p>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <h1>My First App</h1>
+        <p>Happy Coding..!</p>
+        <Avengers />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Avengers.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Avengers extends Component {
+    render () {
+        return (
+            <div className="avenger">
+                <div>Name : Tony Stark</div>
+                <div>HeroName : Iron Man</div>
+                <div>Rank : 1</div>
+            </div>
+        );
+    }
+}
+
+export default Avengers;
