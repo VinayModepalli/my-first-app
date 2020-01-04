bf754b2da5ad0b1864b9b1adfaa791a28404e065
VinayModepalli <vinaykumarmodepalli@gmail.com>
2020-01-04 23:27:55 +0530

    Updates

---
+++ precache-manifest.20bd02e630dbf64349bedbfb74b55af8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc96564c174806eca8d442ca599bb543",
+    "url": "/my-first-app/index.html"
+  },
+  {
+    "revision": "423a0f59c670029e0d52",
+    "url": "/my-first-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aa10bbeb55a8f10bee94",
+    "url": "/my-first-app/static/js/2.ad4eabca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-first-app/static/js/2.ad4eabca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "423a0f59c670029e0d52",
+    "url": "/my-first-app/static/js/main.dfac2b6f.chunk.js"
+  },
+  {
+    "revision": "3b8253ba231e1eaa0ce2",
+    "url": "/my-first-app/static/js/runtime-main.a4067391.js"
+  }
+]);
